1bb922351d896b3bd3642d8196affb74914ab28c
Marina Ruiz Sánchez-Oro <marina_dorada@hotmail.es>
2021-11-22 17:41:03 +0000

    Add functions and several point inputs to scripts

--- automation/find_closest_calibrated_point.py
+++ automation/find_closest_calibrated_point.py
@@ -1,113 +1,158 @@
+#####
+# File to take in a list of points and find where the closest calibrated point is.
+# Find the parameters of the closest calibrated point.
+
+# Author: Marina Ruiz Sanchez-Oro
+# Date: 10/11/2021
+#####
+
 import numpy as np
-#import system
-import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
-from itertools import product
-from datetime import datetime
 import geopandas as gpd
-from shapely.geometry import Point
 from osgeo import gdal, ogr, osr
 from osgeo.gdalnumeric import *
 from osgeo.gdalconst import *
 import pandas as pd
-import numpy as np
-#import shapefile
-import itertools
-import json
 import os
-import rasterio
-from rasterio.features import shapes
-
 import sys
-import fiona
 import image_functions as fn
 from shapely.ops import nearest_points
+from shapely import wkt
+import pyproj
+
+from shapely.geometry import Point
+from shapely.ops import transform
 
 
-#####
-# File to take in a point and find where the closest calibrated point is.
-# Find the parameters of the closest calibrated point.
-#####
-
-
-### IMPORTANT NOTE ### REMEBER THAT SOME POINTS HAVE TWO VALUES, THIS IS WHY
+### IMPORTANT NOTE ### REMEMBER THAT SOME POINTS HAVE TWO VALUES, THIS IS WHY
 ### THE NUMBER OF ROWS IN THE CALIBRATION FILE DOESN'T MATCH THE NUMBER OF
 ### COORDINATE POINTS
+def convert_calib_to_lat_lon(dem_raster, calib_csv, output_calib_raster):
+    dem_file = dem_raster
 
-dem_file = "/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Topography/eu_dem_AoI_epsg32633.bil"
+    # 0. Load rasters into arrays
+    demarr, pixelWidth, (geotransform, inDs) = fn.ENVI_raster_binary_to_2d_array(dem_file)
 
-# 0. Load rasters into arrays
-demarr, pixelWidth, (geotransform, inDs) = fn.ENVI_raster_binary_to_2d_array(dem_file)
+    calibration_file = calib_csv
 
-calibration_file = '/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Calibration/Calibrated_FoS_depth.csv'
+    # need to convert the csv into a geodataframe
+    input_df = pd.read_csv(calibration_file)
+    input_array = 0*demarr
+    for i in range (len(input_df)):
+        x = int(input_df['row'].iloc[i])
+        y = int(input_df['col'].iloc[i])
+        input_array[x,y] = input_df.index[i]
 
-# need to convert the csv into a geodataframe
-input_df = pd.read_csv(calibration_file)
-input_array = 0*demarr
-for i in range (len(input_df)):
-    x = int(input_df['row'].iloc[i])
-    y = int(input_df['col'].iloc[i])
-    input_array[x,y] = input_df.index[i]
+    input_array[input_array == 0] = 'nan'
 
-input_array[input_array == 0] = 'nan'
-
-new_geotransform,new_projection,file_out = fn.ENVI_raster_binary_from_2d_array( (geotransform, inDs), "./_test_transform.bil", pixelWidth, input_array)
-print('I am done yayyy')
-
+    # convert the row,col of the calibration file into lat, lon coordinates
+    new_geotransform,new_projection,file_out = fn.ENVI_raster_binary_from_2d_array( (geotransform, inDs), output_calib_raster, pixelWidth, input_array)
+    print('I am done. The calibration file now has lon, lat coordinates.')
 
 # The following is useful code. KEEP IT!! - just need to uncomment it
-filename='test_transform'
-inDs = gdal.Open('./_test_transform.bil'.format(filename))
-outDs = gdal.Translate('{}.xyz'.format(filename), inDs, format='XYZ', creationOptions=["ADD_HEADER_LINE=YES"])
-outDs = None
-try:
-    os.remove('{}.csv'.format(filename))
-except OSError:
-    pass
-os.rename('{}.xyz'.format(filename), '{}.csv'.format(filename))
-os.system('ogr2ogr -f "ESRI Shapefile" -oo X_POSSIBLE_NAMES=X* -oo Y_POSSIBLE_NAMES=Y* -oo KEEP_GEOM_COLUMNS=NO {0}.shp {0}.csv'.format(filename))
+# Convert the raster file with the calibrated points to a shapefile + csv file
+
+# get the name of the file from the full filepath and extension
+def convert_calib_raster_to_csv_shp(calib_raster):
+    filename_full = os.path.basename(calib_raster)
+    filename = filename_full.split('.')
+    filename = filename[0]
+
+    inDs = gdal.Open(calib_raster)
+    outDs = gdal.Translate('{}.xyz'.format(filename), inDs, format='XYZ', creationOptions=["ADD_HEADER_LINE=YES"])
+    outDs = None
+    try:
+        os.remove('{}.csv'.format(filename))
+    except OSError:
+        pass
+    os.rename('{}.xyz'.format(filename), '{}.csv'.format(filename))
+    os.system('ogr2ogr -f "ESRI Shapefile" -oo X_POSSIBLE_NAMES=X* -oo Y_POSSIBLE_NAMES=Y* -oo KEEP_GEOM_COLUMNS=NO {0}.shp {0}.csv'.format(filename))
+
 
 # This has converted the raster to a csv with the xy coordinates and the Z values at the calibrated points.
 ## We want to split the data now so that we have a lat, lon, Z column and it's easier to sort the data that is not NaN.
 
-# Load the file
-input_df = pd.read_csv('./test_transform.csv', ' ')
-selected_rows = input_df[~input_df['Z'].isnull()]
-selected_rows_indices = selected_rows.index
-print(selected_rows.index)
+def create_calib_multipoint(calib_csv):
+    # Load the .csv file with all the calibrated points in point geometry
+    file_path = os.path.dirname(calib_csv)
+    filename_full = os.path.basename(calib_csv)
 
-geometry = [Point(xy) for xy in zip(selected_rows.X, selected_rows.Y)]
-geo_df = gpd.GeoDataFrame(selected_rows, geometry=geometry)
-print(geo_df.head)
+    #input_df = pd.read_csv('./test_transform.csv', ' ')
+    input_df = pd.read_csv(file_path + '/' + filename_full, ' ')
 
-geo_df = geo_df.drop(columns=['X', 'Y', 'Z'])
-multipoint = geo_df.geometry.unary_union
+    # remove the rows that are NaN
+    selected_rows = input_df[~input_df['Z'].isnull()]
+    selected_rows_indices = selected_rows.index
+    print(selected_rows.index)
+
+    # convert the columns into shapely Point Objects
+    geometry = [Point(xy) for xy in zip(selected_rows.X, selected_rows.Y)]
+    geo_df = gpd.GeoDataFrame(selected_rows, geometry=geometry)
+    print(geo_df.head)
+
+    geo_df = geo_df.drop(columns=['X', 'Y', 'Z'])
+    # create a multipoint object - need this to use the nearest_points function
+    multipoint = geo_df.geometry.unary_union
+    return multipoint, selected_rows
+
+# convert_calib_to_lat_lon('/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Topography/eu_dem_AoI_epsg32633.bil',\
+# '/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Calibration/Calibrated_FoS_depth.csv',\
+# './_test_transform.bil')
+#
+# convert_calib_raster_to_csv_shp('./_test_transform.bil')
+#
+multipoint, selected_rows = create_calib_multipoint('./test_transform.csv')
+
+
+
+
 
 # test point - need to change this to be the output from the lat_lon_area_check.py script.
 # need to also be able to take a list of points instead of just one.
-point = Point(515854, 4.551284e+06)
 
-# this prints out the point closest to our list of points
-## need to figure ou
-closest_cal_point = nearest_points(multipoint, point)[0]
-print(closest_cal_point, nearest_points(multipoint, point)[0])
-#print(closest_cal_point.x)
+def calib_params_closest_point(lat_lon_file, calib_file):
+    points_df = pd.read_csv(lat_lon_file, index_col = None)
+    #point_one = points_df['geometry'][0]
+    #closest_points_df =
+    full_calibration_df = pd.read_csv(calib_file, index_col = None)
 
-# note that Z is not the altitude but the row number in the initial dataframe... need to figure out a better way to factor this in.
-calibration_params_index = list(selected_rows[selected_rows['X']==closest_cal_point.x].index.values)
-print(calibration_params_index)
-calibration_parameters_selection = int(selected_rows['Z'][calibration_params_index])
-print(calibration_parameters_selection)
+    closest_points_df = pd.DataFrame(columns=full_calibration_df.columns[1:])
+    points_df['geometry'] = points_df['geometry'].apply(wkt.loads)
+    points_gdf = gpd.GeoDataFrame(points_df, crs='epsg:4326')
 
-calibration_file = '/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Calibration/Calibrated_FoS_depth.csv'
+    for i in range(len(points_df)):
+        #point = Point(515854, 4.551284e+06)
+        point = points_gdf['geometry'][i]
+        # reproject point - MAYBE PUT THIS IN A SEPARATE FUNCTION
+        wgs84_pt = point
+        AoI_proj = pyproj.CRS('EPSG:32633')
+        wgs84 = pyproj.CRS('EPSG:4326')
+        project =  pyproj.Transformer.from_crs(wgs84, AoI_proj, always_xy=True).transform
+        AoI_point = transform(project, wgs84_pt)
+        print(AoI_point.x, AoI_point.y)
+        point = AoI_point
+        print(point.x, point.y)
 
-full_calibration_df = pd.read_csv(calibration_file)
 
-print(full_calibration_df.head())
-calibration_parameters = full_calibration_df.iloc[[calibration_parameters_selection]]
-print(calibration_parameters)
+        # this prints out the point closest to our list of points
+        closest_cal_point = nearest_points(multipoint, point)[0]
+        print(closest_cal_point, nearest_points(multipoint, point)[0])
+        #print(closest_cal_point.x)
 
-# save the parameters to a new file with only the point we want
-#calibration_parameters = calibration_parameters.drop[0]
-calibration_parameters = calibration_parameters.drop(calibration_parameters.columns[[0]], axis=1)
-calibration_parameters.to_csv('./test_one_point_calibration.csv',index=False)
+        # note that Z is not the altitude but the row number in the initial dataframe... need to figure out a better way to factor this in.
+        calibration_params_index = list(selected_rows[selected_rows['X']==closest_cal_point.x].index.values)
+        calibration_parameters_selection = int(selected_rows['Z'][calibration_params_index])
+
+        calibration_parameters = full_calibration_df.iloc[[calibration_parameters_selection]]
+
+        # save the parameters to a new file with only the point we want
+        #calibration_parameters = calibration_parameters.drop[0]
+        calibration_parameters = calibration_parameters.drop(calibration_parameters.columns[[0]], axis=1)
+
+
+        #calibration_parameters[0]
+        print(calibration_parameters.iloc[0])
+        closest_points_df.loc[i] = calibration_parameters.iloc[0]
+        print(closest_points_df)
+    calibration_parameters.to_csv('./test_closest_calibration_points.csv',index=False)
+
+calib_params_closest_point('./bool_lat_lon.csv', '/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Calibration/Calibrated_FoS_depth.csv')
--- automation/lat_lon_area_check.py
+++ automation/lat_lon_area_check.py
@@ -1,62 +1,72 @@
+#####
+# Script to check whether the list of given coordinates are in the area of
+# interest.
+
+# Author: Marina Ruiz Sanchez-Oro
+# Date 4/11/2021
+
+#####
+
+# ARGS : .CSV FILE WITH THE COORDINATES
+# RETURN : DATAFRAME WITH THE POINTS INSIDE THE AREA.
+
 import numpy as np
-#import system
-import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
-from itertools import product
-from datetime import datetime
 import geopandas as gpd
 from shapely.geometry import Point
 from osgeo import gdal, ogr, osr
 from osgeo.gdalnumeric import *
 from osgeo.gdalconst import *
 import pandas as pd
-import numpy as np
-#import shapefile
-import itertools
 import json
 import os
 import rasterio
-from rasterio.features import shapes
-
 import sys
-import fiona
-#import functions as fn
 
 # input the data file
 ## must have a list of lat lon points
+def is_it_in_aoi(lat_lon_file, output_file):
+    # shapefile with the area of interest
+    AoI_file = gpd.read_file('/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Topography/AoI.shp', index=False)
+    AoI_file = gpd.GeoDataFrame(AoI_file)
+    AoI_file = AoI_file.drop(columns=['id'])
 
-file = gpd.read_file('/exports/csce/datastore/geos/groups/LSDTopoData/FORESEE/Data/Topography/AoI.shp', index=False)
-file = gpd.GeoDataFrame(file)
-file = file.drop(columns=['id'])
+    # coordinate system of the AoI file
+    AoI_crs = AoI_file.crs
 
+    # csv file with the points of interest - to check if they are in area of interest.
+    # THE COORDINATES MUST BE IN THE SAME CRS AS THE AREA OF INTEREST
+    csv_file_path = lat_lon_file
+    csv_df = pd.read_csv(csv_file_path)
 
-csv_file_path = './test_lat_lon.csv'
-csv_df = pd.read_csv(csv_file_path)
+    # convert test points to shapely point geometry
+    geometry = [Point(xy) for xy in zip(csv_df.lon, csv_df.lat)]
 
-geometry = [Point(xy) for xy in zip(csv_df.lon, csv_df.lat)]
+    # assume the projection is the same as the one in the AoI shapefile.
+    geo_df = gpd.GeoDataFrame(csv_df, geometry=geometry)
+    geo_df = geo_df.set_crs(AoI_crs)
 
-# assume the projection is the same as the one in the AoI shapefile.
-geo_df = gpd.GeoDataFrame(csv_df, geometry=geometry)
-# NEED TO POSSIBLY CHANGE THIS SO THAT IT TAKES THE PROJECTION FROM THE AREA OF INTEREST
-geo_df = geo_df.set_crs('epsg:4326')
+    geo_df = geo_df.drop(columns=['lat', 'lon'])
 
-geo_df = geo_df.drop(columns=['lat', 'lon'])
+    print(geo_df.head(5))
 
-print(geo_df.head(5))
+    # aoi_polygon = AoI_file.iloc[0]
 
-# aoi_polygon = file.iloc[0]
-## In first instance we will assume there is aonly one data point in the area of interest.
-## later we will adapt for more than 1 datapoint.
-for i in range(len(geo_df)):
-    test_point = geo_df['geometry'].iloc[i]
-    print(test_point)
-    print(file.contains(test_point))
-    is_in_area = file.contains(test_point)[0]
-    geo_df.at[i,'is_in_area'] = is_in_area
+    for i in range(len(geo_df)):
+        test_point = geo_df['geometry'].iloc[i]
+        print(test_point)
+        print(AoI_file.contains(test_point))
+        is_in_area = AoI_file.contains(test_point)[0]
+        geo_df.at[i,'is_in_area'] = is_in_area
 
-# keep only the columns with the points in the AoI
-geo_df = geo_df[geo_df.is_in_area]
-print(geo_df.head())
+    # keep only the columns with the points in the AoI
+    geo_df = geo_df[geo_df.is_in_area]
+    print(geo_df.head())
+    geo_df['geometry'].to_csv(output_file, index=None)
+
+    return geo_df
+
+dataframe_aoi = is_it_in_aoi('./test_lat_lon.csv', './bool_lat_lon.csv')
+
 
 
 ################################################################################
