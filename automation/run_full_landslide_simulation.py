2c692d29031fb8f4227a8291cc9df2d2f3f1f65b
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-01-07 10:24:41 +0000

    use argparse for command line

--- automation/run_full_landslide_simulation.py
+++ automation/run_full_landslide_simulation.py
@@ -1,14 +1,14 @@
-import json
-import os
-import sys
+import argparse
 
 from .run_individual_landslide_modules import *
-# sys.argv1 is the directory where the data will be stored
-# sys.argv2 is the name of the coordinate input file
-# sys argv3 is the name of the precipitation file
 
 def main():
-    run_landslide_simulation(sys.argv[1], sys.argv[2], sys.argv[3])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rundir',help='directory where the data will be stored')
+    parser.add_argument('coordinates', help='name of the coordinate input file')
+    parser.add_argument('precipitation', help='name of the precipitation file')
+    args = parser.parse_args()
+    run_landslide_simulation(args.rundir, args.coordinates, args.precipitation)
 
 
 if __name__ == '__main__':
