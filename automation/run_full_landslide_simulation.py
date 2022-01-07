df7edcc922189e321d4753db09da72d0c1317368
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-01-07 13:26:59 +0000

    use default coordinate and precipitation file names

--- automation/run_full_landslide_simulation.py
+++ automation/run_full_landslide_simulation.py
@@ -6,13 +6,13 @@ from .run_json import read_paths_file
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('rundir',help='directory where the data will be stored')
-    parser.add_argument('coordinates', help='name of the coordinate input file')
-    parser.add_argument('precipitation', help='name of the precipitation file')
-    parser.add_argument('-c', '--config', default='file_paths_landslide_automation.json',
+    parser.add_argument('-c','--coordinates', default='coords.csv',help='name of the coordinate input file')
+    parser.add_argument('-p','--precipitation', default='rain.csv',help='name of the precipitation file')
+    parser.add_argument('-f', '--file-config', default='file_paths_landslide_automation.json',
                         help='configuration file')
     args = parser.parse_args()
 
-    file_paths = read_paths_file(args.config)
+    file_paths = read_paths_file(args.file_config)
     
     run_landslide_simulation(args.rundir, args.coordinates, args.precipitation, file_paths)
 
